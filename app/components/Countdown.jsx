712b0d45ce0506c4c5989f1341030b826c5ba56f
Sheri Wilson <smgwilson37@gmail.com>
2016-08-22 16:23:39 -0700

    adds navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Countdown = React.createClass ({
+  render: function() {
+    return(
+      <div>
+        <p>I am the countdown component</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass ({
+  render: function() {
+    return(
+      <div>
+        <p>I am the timer component</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Timer;
