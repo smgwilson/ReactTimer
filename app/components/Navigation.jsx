d746383074f68b5129303a386f517e99167443f7
Sheri Wilson <smgwilson37@gmail.com>
2016-08-21 18:46:11 -0700

    initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var { Link, IndexLink } = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Time App</li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight:'bold'}}>Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link" activeStyle={{fontWeight:'bold'}}>Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="https://github.com/smgwilson" target="_blank">Sheri Wilson</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Navigation;
